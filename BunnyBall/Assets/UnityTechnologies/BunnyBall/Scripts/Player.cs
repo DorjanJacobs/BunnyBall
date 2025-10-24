a3136dd101507b72660298cd05fc936b091052b6
DorjanJacobs <djacobs27@academycharterschool.org>
2025-10-24 09:29:57 -0400

    we added jumping to the game

---
+++ BunnyBall/Assets/UnityTechnologies/BunnyBall/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform cameraTransform;
+    public GameManager gameManager;
+    public float speed = 1f;
+    private int jumpForce = 100;
+    private int x = 0;
+
+
+        void Update()
+    {
+        x = x + 1;
+        Debug.Log(message: "hello" + x);
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+        Vector3 direction = forward * moveVertical + right * moveHorizontal;
+        rb.AddForce(direction * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space was pressed");
+            rb.AddForce(Vector3.up * 400);
+        }
+    }
+}
